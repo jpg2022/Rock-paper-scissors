20d1ebf4ff0a8565649f751de92c257102670d39
jpg22 <jpg22@att.net>
2022-09-26 21:30:50 -0500

    2nd push

--- myscripts.js
+++ myscripts.js
@@ -1,17 +1,16 @@
 function playRound(playerSelection, computerSelection){
     var dict = {"rock":0, "paper":1, "scissors":2};
     var playerint, computerint;
-    dict[playerSelection] = playerint;
-    dict[computerSelection]= computerint;  
+    playerint = dict[playerSelection];
+    computerint = dict[computerSelection];
 
     if (playerint == computerint){
-        console.log("Its a tie! With both player and computer choosing " && playerSelection && "!");
+        console.log("Its a tie! With both player and computer choosing " + playerSelection + "!");
 
-    }elseif((playerint + 1) % 3 == computerint){
-        console.log("Computer wins because computer's "  && computerSelection && " beats player's " && playerSelection && "!");
-    }else {
-
-        console.log("Player wins because player's " && playerSelection && " beats computer's " && computerSelection && "!");
+    }else if((playerint + 1) % 3 == computerint){
+        console.log("Computer wins because computer's "  + computerSelection + " beats player's " + playerSelection + "!");
+    }else{
+        console.log("Player wins because player's " + playerSelection + " beats computer's " + computerSelection + "!");
     }
 
 
@@ -21,16 +20,23 @@ function getComputerChoice(){
     const choices1 = ["rock", "paper", "scissors"];
     let computer = Math.floor(Math.random() * 3);
     let compchoice = choices1.at(computer);
-    return compchoice;
+    console.log("computer int " + computer + " computer chose " + compchoice);
+    return compchoice; 
 }
 
-const choices = ["rock", "paper", "scissors"];
-let player = prompt("Enter rock paper or scissors.");
-player = player.toLowerCase();
-if(choices.includes(player) == false){
-    console.log("Please enter valid choice.");
+function game(){
+    var computerwins, playerwins;
+    for(let i = 0; i < 5; i++){
+        const choices = ["rock", "paper", "scissors"];
+        let playerSelection = prompt("Enter rock paper or scissors.");
+        playerSelection = playerSelection.toLowerCase();
+        if(choices.includes(playerSelection) == false){
+            console.log("Please enter valid choice.");
+        }
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        
+    }
+
 }
 
-
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
